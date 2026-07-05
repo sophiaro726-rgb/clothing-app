ac4347c69847160d7faced3ae9a1f3263cf4d285
sophiaro726-rgb <sophia.ro726@gmail.com>
2026-07-05 17:48:29 -0400

    flask editing

--- backend/data.py
+++ backend/data.py
@@ -1,10 +1,18 @@
-from flask import Flask #flask is used as the backend to play as the messenger 
+from flask import Flask, render_template  #flask is used as the backend to play as the messenger 
 #by receiving requests or data and deciding what to do w it
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return 'Server is running'
+def intro():
+    return render_template('intro.html')
+#<link rel="stylesheet" href="{{url_for('static', filename='style.css')}}"> #
+#flask has static files for style sheet 
+@app.route('/review.html')
+def review():
+    return render_template('review.html')
+@app.route('/search.html')
+def search():
+    return render_template('search.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port = 8000) #starts server w this command
