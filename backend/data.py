ced6ce193bcbd1b0b9af4e14b464b8899e41de0f
sophiaro726-rgb <sophia.ro726@gmail.com>
2026-07-10 22:37:54 -0400

    connected postgres & password

--- backend/data.py
+++ backend/data.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template  #flask is used as the backend to play as the messenger 
 #by receiving requests or data and deciding what to do w it
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,5 +16,20 @@ def review():
 def search():
     return render_template('search.html')
 
+#POSTGRES CODE
+hostname = 'localhost'
+database = 'clothing_app' #what i named the db
+username = 'postgres'
+port_id = 5432
+
+conn = psycopg2.connect(
+    host = hostname,
+    dbname = database,
+    user = username,
+    port = port_id
+)
+cur = conn.cursor()
+#conn.close() dont want to shut server
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port = 8000) #starts server w this command & url matches 8000
+    #needs to br at end so python grabs everything before 
