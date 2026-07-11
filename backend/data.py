08b5ba08a073dba2967be3ab33d0c7aff918e866
sophiaro726-rgb <sophia.ro726@gmail.com>
2026-07-11 00:22:23 -0400

    review submissions loaded into sql database

--- backend/data.py
+++ backend/data.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template  #flask is used as the backend to play as the messenger 
 #by receiving requests or data and deciding what to do w it
-import psycopg2
+import psycopg2 #for sql
+from flask import request 
 
 app = Flask(__name__)
 
@@ -16,6 +17,7 @@ def review():
 def search():
     return render_template('search.html')
 
+
 #POSTGRES CODE
 hostname = 'localhost'
 database = 'clothing_app' #what i named the db
@@ -28,8 +30,29 @@ conn = psycopg2.connect(
     user = username,
     port = port_id
 )
-cur = conn.cursor()
+curr = conn.cursor()
+#connecting sql and submitted review 
+@app.route('/submittedreview.php', methods=['POST']) #just post used not get 
+def submitted():
+    rbrand = request.form['brand']
+    rname = request.form['name']
+    rcategory = request.form['category']
+    rsize = request.form['size']
+    rgender = request.form['gender']
+    rcolor = request.form['color']
+    ryear = request.form['year']
+    if ryear == "":
+        ryear =  None #do this bc int type and causes an error
+    #photo dif bc file 
+    f = request.files['photo'] #diff bc file 
+    f.save('static/photos/' + f.filename) #dont want to hardcode name of file so not quotes to save into uploads file for flask 
+    rphoto = f.filename
+    curr.execute("INSERT INTO clothesinfo (brand, name, category, size, gender, color, year, photo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (rbrand, rname, rcategory, rsize, rgender, rcolor, ryear, rphoto))
+    # columns, input values and not hard code, and then the variables being input 
+    conn.commit()
+    return "the test worked"
+
 #conn.close() dont want to shut server
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port = 8000) #starts server w this command & url matches 8000
-    #needs to br at end so python grabs everything before 
+    #needs to be at end so python grabs everything before 
